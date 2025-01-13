b04135cd1d731cb1bbd9466029ac326ab299910d
Kareem <kareemy9000@gmail.com>
2025-01-13 22:21:58 +0400

    refactor: Reorder imports in PricingComponent

--- src/pages/Home/PricingComponent.jsx
+++ src/pages/Home/PricingComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import './PricingComponent.css';
+import React, { useState } from 'react';
 import { Box, Typography, Button } from '@mui/joy';
 
 const PricingComponent = () => {
